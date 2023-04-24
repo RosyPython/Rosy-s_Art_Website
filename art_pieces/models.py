8f0c482e1948d4724b2c346956c5a29541fe5b3d
Jessica Corbyn <rosycapetown@protonmail.com>
2023-04-24 20:44:43 +0100

    Set up database for art

--- art_pieces/models.py
+++ art_pieces/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ArtPiece(models.Model):
+    name = models.CharField(max_length=255)
+    date_finished = models.DateTimeField()
