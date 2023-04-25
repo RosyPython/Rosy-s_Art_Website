98911544e01c8cb8c6748cf4981b7fd2f73f9bfe
Jessica Corbyn <rosycapetown@protonmail.com>
2023-04-25 19:58:24 +0100

    Properly set up drawings page

--- Rosys_Art/urls.py
+++ Rosys_Art/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('drawings/', include('art_pieces.urls'))
+    path('', include('art_pieces.urls'))
 ]
